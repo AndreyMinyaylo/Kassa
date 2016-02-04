1f18fd02e9b14f9ab9c1eee584e800860e80b71a
AndreyMinyaylo <minyailoandrey@gmail.com>
2016-02-04 10:12:03 +0200

    rename

--- Pifagor/Program.cs
+++ Pifagor/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pifagor
 {
@@ -11,43 +7,43 @@ namespace Pifagor
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter the first number of the horizontal array of Pythagoras:");
+            Console.WriteLine("Enter the first number (horizontal): ");
             var firstNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the last number of the horizontal array of Pythagoras:");
+            Console.WriteLine("Enter the last number (horizontal): ");
             var secondNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the first number of a vertical array of Pythagoras:");
+            Console.WriteLine("Enter the first number (vertical): ");
             var thirdNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the last number of vertical array of Pythagoras:");
+            Console.WriteLine("Enter the last number (vertical): ");
             var fourthNumber = Convert.ToInt32(Console.ReadLine());
 
             int i, j;
 
             
-            var lenth = Convert.ToString(fourthNumber).Length + 1;
-            while (lenth > 0)
+            var space = Convert.ToString(fourthNumber).Length + 1;
+            while (space > 0)
             {
                 Console.Write(" ");
-                lenth--;
+                space--;
             }
             
             Console.Write("|");
                 for (i = firstNumber; i <= secondNumber; i++)
                 {
-                  var LenghtString = Convert.ToString(i).Length;
-                  var productColumn = secondNumber * fourthNumber;
-                  var LenghtColumn = Convert.ToString(productColumn).Length;
-                    if (LenghtColumn == LenghtString)
+                  var lenghtI = Convert.ToString(i).Length;
+                  var symbol = secondNumber * fourthNumber;
+                  var symbolLenght = Convert.ToString(symbol).Length;
+                    if (symbolLenght == lenghtI)
                     {
-                    LenghtColumn = 1;
+                    symbolLenght = 1;
                     }
-                    else if (LenghtString < LenghtColumn)
+                    else if (lenghtI < symbolLenght)
                     {
-                    LenghtColumn = (LenghtColumn - LenghtString) + 1;
+                    symbolLenght = (symbolLenght - lenghtI) + 1;
                     }
-                while (LenghtColumn > 0) 
+                while (symbolLenght > 0) 
                 {
                     Console.Write(" ");
-                    LenghtColumn--;
+                    symbolLenght--;
                     }
                 
                     Console.Write(i);
@@ -63,12 +59,12 @@ namespace Pifagor
             Console.Write("+");
             for (i = firstNumber; i <= secondNumber; i++)
             {
-                var productEndVar = secondNumber * fourthNumber;
-                var LenghtproductEndVar = Convert.ToString(productEndVar).Length + 1;
-                while (LenghtproductEndVar > 0)
+                var symbol = secondNumber * fourthNumber;
+                var symbolLenght = Convert.ToString(symbol).Length + 1;
+                while (symbolLenght > 0)
                 {
                     Console.Write("-");
-                    LenghtproductEndVar--;
+                    symbolLenght--;
                 }
                 
             }
@@ -94,26 +90,26 @@ namespace Pifagor
                 Console.Write(i+"|");
                 for (j = firstNumber; j <= secondNumber; j++)
                 {
-                    var Result = i * j;
-                    var product = Convert.ToString(Result).Length;
-                    var productColumn = secondNumber * fourthNumber;
-                    var lenghtproductEndVar = Convert.ToString(productColumn).Length;
-                    if (product == lenghtproductEndVar)
+                    var result = i * j;
+                    var resultLenght = Convert.ToString(result).Length;
+                    var symbol = secondNumber * fourthNumber;
+                    var symbolLenght = Convert.ToString(symbol).Length;
+                    if (resultLenght == symbolLenght)
                     {
-                        lenghtproductEndVar =  1;
+                        symbolLenght =  1;
                     }
-                    else if(product < lenghtproductEndVar)
+                    else if(resultLenght < symbolLenght)
                     {
-                        lenghtproductEndVar = (lenghtproductEndVar - product) +1;
+                        symbolLenght = (symbolLenght - resultLenght) +1;
                     }
 
-                    while (lenghtproductEndVar > 0)
+                    while (symbolLenght > 0)
                     {
                             Console.Write(" ");
-                        lenghtproductEndVar--;
+                        symbolLenght--;
                     }
                     
-                        Console.Write(Result);
+                        Console.Write(result);
 
                     if (j == secondNumber)
                     {
