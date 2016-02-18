0e1a959f893e9f11126403f7d11c68ab5a12db0c
AndreyMinyaylo <minyailoandrey@gmail.com>
2016-02-18 12:31:19 +0200

    TryParse

--- Pifagor/Program.cs
+++ Pifagor/Program.cs
@@ -1,142 +1,198 @@
 ﻿using System;
 using Microsoft.Win32;
+using System.Text;
 
 namespace Pifagor
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var firtNumberHorizontal = -1;
+            var lastNumberHorizontal = -1;
+            var firstNumberVertical = -1;
+            var lastNumberVertical = -1;
+            string key;
             Console.WriteLine("You must enter numbers above than 0");
-            Console.WriteLine("Enter the first number (horizontal): ");
-            var firstNumberHorizontal = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the last number (horizontal): ");
-            var lastNumberHorizontal = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the first number (vertical): ");
-            var firstNumberVertical = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the last number (vertical): ");
-            var lastNumberVertical = Convert.ToInt32(Console.ReadLine());
-
-            if (firstNumberHorizontal < 0)
-            {
-                Console.WriteLine("The firstNumberHorizonta isnt above or equally 0");
-                return;
-            }
-            if (lastNumberHorizontal < 0)
-            {
-                Console.WriteLine("The lastNumberHorizonta isnt above or equally 0");
-                return;
-            }
-            if (firstNumberVertical < 0)
-            {
-                Console.WriteLine("The firstNumberVertical isnt above or equally 0");
-                return;
-            }
-            if (lastNumberVertical < 0)
-            {
-                Console.WriteLine("The lastNumberVertical isnt above or equally 0");
-                return;
-            }
-
-            var space = Convert.ToString(lastNumberVertical).Length + 1;
-            while (space > 0)
-            {
-                Console.Write(" ");
-                space--;
-            }
             
-            Console.Write("|");
-                for (int i = firstNumberHorizontal; i <= lastNumberHorizontal; i++)
-                {
-                  var lenghtI = Convert.ToString(i).Length;
-                  var symbol = lastNumberHorizontal * lastNumberVertical;
-                  var symbolLenght = Convert.ToString(symbol).Length;
-                    if (symbolLenght == lenghtI)
-                    {
-                    symbolLenght = 1;
-                    }
-                    else if (lenghtI < symbolLenght)
-                    {
-                    symbolLenght = (symbolLenght - lenghtI) + 1;
-                    }
-                while (symbolLenght > 0) 
-                {
-                    Console.Write(" ");
-                    symbolLenght--;
-                    }
-                
-                    Console.Write(i);
-                    }
-            Console.Write("\n");
-            var lenthHorizontal = Convert.ToString(lastNumberVertical).Length + 1;
-            while (lenthHorizontal > 0)
-            {
-                Console.Write("-");
-                lenthHorizontal--;
-            }
-            
-            Console.Write("+");
-            for (int i = firstNumberHorizontal; i <= lastNumberHorizontal; i++)
-            {
-                var symbol = lastNumberHorizontal * lastNumberVertical;
-                var symbolLenght = Convert.ToString(symbol).Length + 1;
-                while (symbolLenght > 0)
-                {
-                    Console.Write("-");
-                    symbolLenght--;
-                }
-                
-            }
-            Console.Write("\n");
-            for (int i = firstNumberVertical; i <= lastNumberVertical; i++)
-            {
-                var lenthI = Convert.ToString(i).Length;
-                var lenthForNumber = Convert.ToString(lastNumberVertical).Length;
-                if (lenthI == lenthForNumber)
-                {
-                    lenthForNumber = 1;
-                }
-                else if (lenthI < lenthForNumber)
-                {
-                    lenthForNumber = (lenthForNumber - lenthI) + 1;
-                }
-                while (lenthForNumber > 0)
-                {
-                    Console.Write(" ");
-                    lenthForNumber--;
-                }
-                
-                Console.Write(i+"|");
-                for (int j = firstNumberHorizontal; j <= lastNumberHorizontal; j++)
-                {
-                    var result = i * j;
-                    var resultLenght = Convert.ToString(result).Length;
-                    var symbol = lastNumberHorizontal * lastNumberVertical;
-                    var symbolLenght = Convert.ToString(symbol).Length;
-                    if (resultLenght == symbolLenght)
-                    {
-                        symbolLenght =  1;
-                    }
-                    else if(resultLenght < symbolLenght)
-                    {
-                        symbolLenght = (symbolLenght - resultLenght) +1;
-                    }
 
-                    while (symbolLenght > 0)
-                    {
-                            Console.Write(" ");
-                        symbolLenght--;
-                    }
+            do
+            {
+                Console.WriteLine("Enter the first number (horizontal): ");
+                key = Console.ReadLine();
+            }
+            while (!Int32.TryParse(key, out firtNumberHorizontal));
+            
+
+            if (firtNumberHorizontal < 0)
+                {
+                    Console.WriteLine("The firstNumberHorizonta isnt above or equally 0");
                     
-                        Console.Write(result);
-
-                    if (j == lastNumberHorizontal)
-                    {
-                        Console.Write("\n");
-                    }
                 }
+            
+            
+            do
+            {
+                Console.WriteLine("Enter the last number (horizontal): ");
+                key = Console.ReadLine();
             }
-            Console.ReadKey();
-        }
-    }
-}
+            while (!Int32.TryParse(key, out lastNumberHorizontal));
+            
+
+                if (lastNumberHorizontal < 0)
+                {
+                    Console.WriteLine("The lastNumberHorizontal isnt above or equally 0");
+                   
+                }
+            
+            
+            do
+            {
+                Console.WriteLine("Enter the first number (vertical): ");
+                key = Console.ReadLine();
+            }
+            while (!Int32.TryParse(key, out firstNumberVertical));
+            
+
+                if (firstNumberVertical < 0)
+                {
+                    Console.WriteLine("The firstNumberVertical isnt above or equally 0");
+                   
+                }
+            
+
+            
+            do
+            {
+                Console.WriteLine("Enter the last number (vertical): ");
+                key = Console.ReadLine();
+            }
+            while (!Int32.TryParse(key, out lastNumberVertical));
+            
+
+                if (lastNumberVertical < 0)
+                {
+                    Console.WriteLine("The lastNumberVertical isnt above or equally 0");
+                    
+                }
+            
+
+           /*if (lastNumberHorizontal < 0)
+                                {
+                                    Console.WriteLine("The lastNumberHorizonta isnt above or equally 0");
+                                    return;
+                                }
+                                if (firstNumberVertical < 0)
+                                {
+                                    Console.WriteLine("The firstNumberVertical isnt above or equally 0");
+                                    return;
+                                }
+                                if (lastNumberVertical < 0)
+                                {
+                                    Console.WriteLine("The lastNumberVertical isnt above or equally 0");
+                                    return;
+                                }*/
+
+                                var space = Convert.ToString(lastNumberVertical).Length + 1;
+                                while (space > 0)
+                                {
+                                    Console.Write(" ");
+                                    space--;
+                                }
+
+                                Console.Write("|");
+                                for (int i = firtNumberHorizontal; i <= lastNumberHorizontal; i++)
+                                {
+                                    var lenghtI = Convert.ToString(i).Length;
+                                    var symbol = lastNumberHorizontal*lastNumberVertical;
+                                    var symbolLenght = Convert.ToString(symbol).Length;
+                                    if (symbolLenght == lenghtI)
+                                    {
+                                        symbolLenght = 1;
+                                    }
+                                    else if (lenghtI < symbolLenght)
+                                    {
+                                        symbolLenght = (symbolLenght - lenghtI) + 1;
+                                    }
+                                    while (symbolLenght > 0)
+                                    {
+                                        Console.Write(" ");
+                                        symbolLenght--;
+                                    }
+
+                                    Console.Write(i);
+                                }
+                                Console.Write("\n");
+                                var lenthHorizontal = Convert.ToString(lastNumberVertical).Length + 1;
+                                while (lenthHorizontal > 0)
+                                {
+                                    Console.Write("-");
+                                    lenthHorizontal--;
+                                }
+
+                                Console.Write("+");
+                                for (int i = firtNumberHorizontal; i <= lastNumberHorizontal; i++)
+                                {
+                                    var symbol = lastNumberHorizontal*lastNumberVertical;
+                                    var symbolLenght = Convert.ToString(symbol).Length + 1;
+                                    while (symbolLenght > 0)
+                                    {
+                                        Console.Write("-");
+                                        symbolLenght--;
+                                    }
+
+                                }
+                                Console.Write("\n");
+                                for (int i = firstNumberVertical; i <= lastNumberVertical; i++)
+                                {
+                                    var lenthI = Convert.ToString(i).Length;
+                                    var lenthForNumber = Convert.ToString(lastNumberVertical).Length;
+                                    if (lenthI == lenthForNumber)
+                                    {
+                                        lenthForNumber = 1;
+                                    }
+                                    else if (lenthI < lenthForNumber)
+                                    {
+                                        lenthForNumber = (lenthForNumber - lenthI) + 1;
+                                    }
+                                    while (lenthForNumber > 0)
+                                    {
+                                        Console.Write(" ");
+                                        lenthForNumber--;
+                                    }
+
+                                    Console.Write(i + "|");
+                                    for (int j = firtNumberHorizontal; j <= lastNumberHorizontal; j++)
+                                    {
+                                        var result = i*j;
+                                        var resultLenght = Convert.ToString(result).Length;
+                                        var symbol = lastNumberHorizontal*lastNumberVertical;
+                                        var symbolLenght = Convert.ToString(symbol).Length;
+                                        if (resultLenght == symbolLenght)
+                                        {
+                                            symbolLenght = 1;
+                                        }
+                                        else if (resultLenght < symbolLenght)
+                                        {
+                                            symbolLenght = (symbolLenght - resultLenght) + 1;
+                                        }
+
+                                        while (symbolLenght > 0)
+                                        {
+                                            Console.Write(" ");
+                                            symbolLenght--;
+                                        }
+
+                                        Console.Write(result);
+
+                                        if (j == lastNumberHorizontal)
+                                        {
+                                            Console.Write("\n");
+                                        }
+                                    }
+                                }
+                                Console.ReadKey();
+                            }
+                        }
+                    }
+                
